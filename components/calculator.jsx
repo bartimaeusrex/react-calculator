c9467329e02474d14c072c61a4484d0a13812aeb
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-05-23 00:42:23 -0400

    Mostly done

---
+++ components/calculator.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+export default () => {
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [oper, setOper] = useState('')
+  const [calc, setCalc] = useState('')
+  const [message, setMessage] = useState('')
+
+  const updateNum1 = event => setNum1(event.target.value)
+  const updateNum2 = event => setNum2(event.target.value)
+  const updateOper = event => setOper(event.target.value)
+
+  const updateCalc = (event) => {
+    if (Number(num1) && Number(num2)) {
+      switch (oper) {
+        case 'add':
+          setCalc(Number(num1) + Number(num2))
+          break;
+        case 'subtract':
+          setCalc(Number(num1) - Number(num2))
+          break;
+        case 'multiply':
+          setCalc(Number(num1) * Number(num2))
+          break;
+        case 'divide':
+          setCalc(Number(num1) / Number(num2))
+          break;
+        default:
+          setCalc('')
+    }
+  } else {
+    setMessage('Please provide proper numbers. Thanks.')
+    setCalc('')
+  }
+  }
+
+  return (
+    <div className="page">
+      <div className="title">React Calculator</div>
+      <input type="text" className="num" onChange={updateNum1} />
+      <select id="operator" onChange={updateOper}>
+        <option value="add">+</option>
+        <option value="subtract">-</option>
+        <option value="multiply">*</option>
+        <option value="divide">/</option>
+      </select>
+      <input type="text" className="num" onChange={updateNum2} />
+      <button onClick={updateCalc}>=</button>
+      <input type="text" className="num" value={calc} />
+      <div className="message">
+        {message ? `${message}` : null}
+      </div>
+    </div>
+  )
+}
